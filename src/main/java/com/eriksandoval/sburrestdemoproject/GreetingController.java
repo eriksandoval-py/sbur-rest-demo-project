63cebe02f34759cc68c5bb8dde3a3909902e80e8
Erik Sandoval <eriksandoval.py@gmail.com>
2023-07-13 14:07:57 -0500

    Created GreetingController.

---
+++ src/main/java/com/eriksandoval/sburrestdemoproject/GreetingController.java
@@ -0,0 +1,21 @@
+package com.eriksandoval.sburrestdemoproject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @Value("${greeting-name: Mirage}")
+    private String name;
+
+    @GetMapping
+    String getGreeting() {
+        return name;
+    }
+
+    
+}
