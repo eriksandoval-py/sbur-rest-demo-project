581a790c2aae19935a23c4d4734d60478b738d7e
Erik Sandoval <eriksandoval.py@gmail.com>
2023-07-15 11:45:18 -0500

    Added config prop scan to main class.

--- src/main/java/com/eriksandoval/sburrestdemoproject/SburRestDemoProjectApplication.java
+++ src/main/java/com/eriksandoval/sburrestdemoproject/SburRestDemoProjectApplication.java
@@ -6,7 +6,9 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class SburRestDemoProjectApplication {
 
